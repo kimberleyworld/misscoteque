0eb522df7ab304d51379de6e89deb1a2e7fd93cb
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-11-24 18:34:37 +0000

    added music player

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import ThreeDObject from "@/app/ui/ThreeDObject"
 import ComingSoon from "@/app/ui/ComingSoon";
 import MailerLiteForm from "./ui/MailerLiteForm";
+import MusicPlayer from "./ui/MusicPlayer";
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center bg-zinc-50">
       <ThreeDObject />
-      <h1 className="absolute top-3/5 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl font-bold" >Misscoteque</h1>
       <ComingSoon/>
+      <h1 className="absolute top-107 left-1/2 -translate-x-1/2 text-5xl text-black font-bold" >Misscoteque</h1>
+      <MusicPlayer />
       <MailerLiteForm />
     </main>
   );
