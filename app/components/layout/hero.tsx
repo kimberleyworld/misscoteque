605bd5968e3038bde3b40a3b7e340998ce7e1e85
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2026-05-25 20:26:58 +0100

    added list to main page

--- app/components/layout/hero.tsx
+++ app/components/layout/hero.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 export default function Hero() {
   return (
@@ -50,9 +52,26 @@ export default function Hero() {
         </div>
       </div> */}
 
-      {/* Flexbox with heading - sits above cut-out */}
+      {/* Scroll to see - positioned at top right */}
+      <div className="absolute top-1 right-2 md:top-6 md:right-6 z-20 text-black flex flex-col items-end text-right">
+        <div>Scroll to see:</div>
+        <ul className="list-disc list-inside flex flex-col text-sm md:text-base font-bold">
+          <li>Upcoming Events</li>
+          <li>Community Notice Board</li>   
+          <li>Gay Crossword</li>
+          <li>Poster Gallery</li>
+          ...and more
+        </ul>
+        <Link href="/artifacts">
+          <Button className="mt-4">
+            View the Archive →
+          </Button>
+        </Link>
+      </div>
+
+      {/* Flexbox with heading */}
       <div className="flex w-full h-full items-center justify-start md:justify-end md:items-end">
-            <h1 className="text-black -rotate-90 md:rotate-0 md:translate-x-0 z-20 text-5xl -translate-x-[40%] text-red-outline md:pb-16 md:text-8xl">MISSCOTEQUE.WORLD</h1>
+        <h1 className="text-black -rotate-90 md:rotate-0 md:translate-x-0 z-20 text-5xl -translate-x-[40%] text-red-outline md:pb-16 md:text-8xl">MISSCOTEQUE.WORLD</h1>
       </div>
       {/* Cut-out image - at bottom of hero section */}
       <div className="absolute bottom-0 left-0 right-0 z-0 w-full h-3/5 sm:h-4/5 overflow-hidden bg-red">
