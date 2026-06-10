eb421b259adee5ee9a80d1dcc71bf2b98b9dcb50
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2026-06-10 11:47:31 +0100

    fixed mobile horizotnal layout breaking

--- app/components/layout/about-section.tsx
+++ app/components/layout/about-section.tsx
@@ -5,9 +5,9 @@ interface AboutSectionProps {
 
 export function AboutSection({ title, copy }: AboutSectionProps) {
   return (
-    <div className="w-full gap-3 flex flex-row justify-center items-center bg-red p-8">
-      <h2 className="text-5xl font-bold flex-none w-1/3 md:w-1/6">{title}</h2>
-      <p className="text-sm overflow-hidden flex">
+    <div className="w-full gap-3 flex flex-col md:flex-row justify-center items-center bg-red p-8">
+      <h2 className="text-5xl font-bold flex-none w-full md:w-1/3 md:w-1/6">{title}</h2>
+      <p className="text-sm break-words w-full md:w-auto">
         {copy}
       </p>
     </div>
