793b861a7e4476552fe674e94ef565cb9cbd0dc8
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2026-08-23 10:37:52 +0100

    text bg edit

--- app/components/layout/about-section.tsx
+++ app/components/layout/about-section.tsx
@@ -7,7 +7,7 @@ export function AboutSection({ title, copy }: AboutSectionProps) {
   return (
     <div className="w-full gap-3 flex flex-col md:flex-row justify-center items-center bg-red p-8">
       <h2 className="text-5xl font-bold flex-none w-full md:w-1/3 md:w-1/6">{title}</h2>
-      <p className="text-sm break-words w-full md:w-auto">
+      <p className="text-sm break-words text-cream bg-black px-2 w-full md:w-auto">
         {copy}
       </p>
     </div>
